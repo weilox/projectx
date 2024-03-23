6b97902ff9ff73736040a6092a8bb696fec8fdd8
weilox <142926069+weilox@users.noreply.github.com>
2024-03-23 04:25:32 +0300

    xxx45

--- room.php
+++ room.php
@@ -4,12 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/room.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Room x</title>
 </head>
 <body>
     <main>
         <aside class="users">
-
+            <div class="user">
+                <i class="fa-solid fa-user"></i>
+                <p>user1</p>
+            </div>
+            <div class="user">
+                <i class="fa-solid fa-user"></i>
+                <p>user2</p>
+            </div>
+            <div class="user">
+                <i class="fa-solid fa-user"></i>
+                <p>user3</p>
+            </div>
         </aside>
         <div class="content">
             <article class="chat" id="chat">
