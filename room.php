03d72e812fcae2d680ef6b1545975c09a21efa09
weilox <142926069+weilox@users.noreply.github.com>
2024-03-23 18:58:10 +0300

    new

--- room.php
+++ room.php
@@ -8,58 +8,6 @@
     <title>Room x</title>
 </head>
 <body>
-<script>
-function chatInit() {
-  const chatDiv = document.querySelector('.chat');
-  // Önceki mesajları temizle
-  chatDiv.innerHTML = '';
-  
-  // Daha önce yüklenip yüklenmediğini kontrol et
-  if (!chatInit.initialized) {
-    // messages.txt dosyasından veriyi al ve messagesDiv içine ekle
-    fetch('messsages.txt')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text();
-      })
-      .then(data => {
-        // messages.txt dosyasından alınan veriyi parçalara ayır
-        const messagesArray = data.split('\n');
-
-        // Her bir metin parçası için yeni bir messages divi oluştur ve içine ekle
-        messagesArray.forEach(message => {
-          const messageDiv = document.createElement('div');
-          const p = document.createElement('p');
-          messageDiv.classList.add('message');
-          messageDiv.appendChild(p);
-
-          p.textContent = message;
-          chatDiv.appendChild(messageDiv);
-        });
-      })
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-      });
-
-    // Chat fonksiyonunun daha önce başlatıldığını işaretle
-    
-  }
-}
-
-// Belirli bir süre sonra tekrar çağır
-setInterval(chatInit, 1000);
-
-
-const chat = document.getElementById('chat');
-chat.scrollTop = chat.scrollHeight;
-
-window.onload = function() {
-  chat.scrollTop = chat.scrollHeight;
-};
-</script>
-
     <form action="room.php" method="POST">
     <main>
         <aside class="users">
@@ -86,17 +34,15 @@ window.onload = function() {
             </article>
         </div>
     </main>
+    <script src="/scripts/room.js"></script>
 </form>
 </body>
 </html>
 
 <?php
-
 if($_POST["text"]){
     $text = $_POST["text"];
     
     file_put_contents("messsages.txt",$text .PHP_EOL, FILE_APPEND);
 }
-
-
 ?>
